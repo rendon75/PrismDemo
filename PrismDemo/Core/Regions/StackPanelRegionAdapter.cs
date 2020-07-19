96194faae7da59519d047d17e19df54e72fbf2c5
Rene Rendon Jr <rene.rendon@hotmail.com>
2020-07-18 22:44:07 -0500

    first commit

---
+++ PrismDemo/Core/Regions/StackPanelRegionAdapter.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Regions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrismDemo.Core.Regions
+{
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(RegionBehaviorFactory behaviorFactory)
+            : base(behaviorFactory)
+        {
+
+        }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach (FrameworkElement item in e.NewItems)
+                    {
+                        regionTarget.Children.Add(item);
+                    }
+                }
+                else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (FrameworkElement item in e.OldItems)
+                    {
+                        regionTarget.Children.Remove(item);
+                    }
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
