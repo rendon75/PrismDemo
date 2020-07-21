1fd7364e50b1abca97583491752ad239427a9eac
Rene Rendon Jr <rene.rendon@hotmail.com>
2020-07-20 21:51:46 -0500

    First view.

--- ModuleA/ModuleAModule.cs
+++ ModuleA/ModuleAModule.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Ioc;
+﻿using ModuleA.Views;
+using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +10,16 @@ namespace ModuleA
 {
     public class ModuleAModule : IModule
     {
+        private IRegionManager _regionMananger;
+
+        public ModuleAModule(IRegionManager regionManager)
+        {
+            _regionMananger = regionManager;    
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            _regionMananger.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
