37d9dd7fc890ec88466e8c408c54ae7dc98e7b6e
Rene Rendon Jr <rene.rendon@hotmail.com>
2020-07-22 22:13:38 -0500

    Used view injection.

--- ModuleA/ModuleAModule.cs
+++ ModuleA/ModuleAModule.cs
@@ -5,6 +5,7 @@ using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace ModuleA
 {
@@ -19,7 +20,29 @@ namespace ModuleA
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
-            _regionMananger.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+            IRegion region = _regionMananger.Regions["ContentRegion"];
+
+            var view1 = containerProvider.Resolve<ViewA>();
+            region.Add(view1);
+
+            var view2 = containerProvider.Resolve<ViewA>();
+            view2.Content = new TextBlock
+            {
+                Text = "Hello from View 2",
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+            region.Add(view2);
+            region.Activate(view2);
+
+            region.Activate(view1);
+            region.Deactivate(view1);
+
+            region.Activate(view2);
+            region.Remove(view2);
+
+            region.Activate(view1);
+
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
