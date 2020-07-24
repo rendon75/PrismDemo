a3fe59802a31d72fa8efa72342d4f3d6f06ba1e7
Rene Rendon Jr <rene.rendon@hotmail.com>
2020-07-24 18:03:15 -0500

    Added ViewModel using default convention.

---
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _title = "Hello from ViewAViewModel";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+    }
+}
